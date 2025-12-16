9d0afdb7820440a327c75f75ebff9523e7d84e72
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-12-16 19:31:30 +0200

    Also make LocalesColumn work in a relation manager

--- src/Tables/LocalesColumn.php
+++ src/Tables/LocalesColumn.php
@@ -3,6 +3,7 @@
 namespace Codedor\TranslatableTabs\Tables;
 
 use Closure;
+use Exception;
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Contracts\HasTable;
 use Illuminate\Support\Str;
@@ -36,10 +37,16 @@ class LocalesColumn extends Column
 
     public function getResourceUrl(string $locale): string
     {
+        /** @var \Filament\Resources\Pages\Page&HasTable $livewire */
         $livewire = $this->getLivewire();
 
-        /** @var \Filament\Resources\Pages\Page&HasTable $livewire */
-        $resource = $livewire::getResource();
+        if (method_exists($livewire, 'getResource')) {
+            $resource = $livewire->getResource();
+        } else if (method_exists($livewire, 'getRelatedResource')) {
+            $resource = $livewire->getRelatedResource();
+        } else {
+            throw new Exception('Can not find a resource, make sure you are in a resource page or relation manager');
+        }
 
         /** @var \Filament\Resources\Resource $resource */
         $url = $resource::getUrl($this->resourceAction, [
